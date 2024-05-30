adc92946b64712c2a3b504a0d29691589758ab9b
BILAL OUQDA <bilalouqda348@gmail.com>
2024-05-30 19:28:40 +0100

    posts

--- routes/postsRoute.js
+++ routes/postsRoute.js
@@ -5,16 +5,16 @@ const router = express.Router()
 const fs = require('fs');
 const multer = require('multer');
 
-const upload = multer({ dest: 'public/images' }); // Set up multer
-//Récupérer et enregistrer les posts dans data.json
+const upload = multer({ dest: 'public/images' });
 
+//Récupérer et enregistrer les posts dans data.json
 router.post('/', async (req, res) => {
   try {
     const response = await userService.getPosts();
     const posts = response.data.slice(0, 10);
     fs.writeFile('data.json', JSON.stringify(posts, null, 2), (err) => {
       if (err) return res.status(500).send('Err');
-      res.send('Posts enregistrés dans data.json');
+      res.send('Posts enrgst dans data.json');
     });
   } catch (error) {
     res.status(500).send('Erreur de serveur');
@@ -38,14 +38,14 @@ router.get('/:postId', (req, res) => {
     if (err) return res.status(500).send('Err');
     const posts = JSON.parse(data);
     const post = posts.find(p => p.id == postId);
-    if (!post) return res.status(404).send('Post non trouvé');
+    if (!post) return res.status(404).send('err post non trouvé');
     res.json(post);
   });
 });
 
 //Créer un fichier dans le dossier public/images
 router.post('/files', upload.single('file'), (req, res) => {
-  res.send('Fichier téléchargé avec succès');
+  res.send('succès');
 });
 
 module.exports = router
